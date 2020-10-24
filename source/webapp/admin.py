f3ef371288158c1b324e07651ef12c8ad150a060
Beknasar <680633@gmail.com>
2020-10-24 11:38:00 +0600

    Создан модель фотографии. Модель фотографии зарегистрирован в админке.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from webapp.models import Photo
+
 
 # Register your models here.
+admin.site.register(Photo)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(upload_to='user_pics', verbose_name='Аватар')
+    signature = models.CharField(max_length=100, verbose_name='Название')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=1,
+                               related_name='photos', verbose_name='Автор')
+
+
